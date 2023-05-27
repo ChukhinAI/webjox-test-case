e4417f52e95308da4600f5cd132abef991beedfb
ChukhinAI <andrey.rnd.96@yandex.ru>
2023-05-27 17:47:25 +0300

    - add counter for posts

--- app/Http/Controllers/Admin/HomeController.php
+++ app/Http/Controllers/Admin/HomeController.php
@@ -4,11 +4,21 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Post;
+use App\Models\User;
+use Illuminate\Database\Eloquent\Model;
 
 class HomeController extends Controller
 {
     public function index()
     {
-        return view('admin.home.index');
+        $posts_published = Post::where('published', '=', '1' )->count(); // 1 == true
+        //$posts_count = Post::all();
+        //$users = User::where('id', '>', 1)->get(); // ok
+        //dd($posts_published);
+        //dd($posts_published);
+        return view('admin.home.index', [
+            'posts_published' => $posts_published,
+        ]);
     }
 }
--- resources/views/admin/home/index.blade.php
+++ resources/views/admin/home/index.blade.php
@@ -32,9 +32,9 @@
                         <!-- small box -->
                         <div class="small-box bg-info">
                             <div class="inner">
-                                <h3>150</h3>
+                                <h3>{{ $posts_published }}</h3>
 
-                                <p>New Orders</p>
+                                <p>Posts published</p>
                             </div>
                             <div class="icon">
                                 <i class="ion ion-bag"></i>
